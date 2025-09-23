09b06a38338617c364ae833fbd518835dd5437df
HY445 <yochi.candy@gmail.com>
2025-09-23 22:35:33 +0900

    kadai_007をアップロードしました。

---
+++ kadai_007/associative_array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP課題2</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $produocts_1 = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($produocts_1);
+    ?>
+  </p>
+</body>
+</html>
